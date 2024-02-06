316c9883a6aa5ad30320d0b9456044ec04304724
Jonas Thiem <jonthiem@gmail.com>
2024-02-06 23:16:55 +0100

    Remove "test" directory

--- web/src/api/library.ts
+++ web/src/api/library.ts
@@ -1,5 +1,7 @@
 import { z } from "zod"
 
+import { Response, post } from "./api"
+
 export const LibraryListsSchema = z.enum([
 	"listening",
 	"want to listen",
@@ -7,3 +9,11 @@ export const LibraryListsSchema = z.enum([
 	"abandoned",
 ])
 export type LibraryLists = z.infer<typeof LibraryListsSchema>
+
+// Add a book to user's library.
+export const addBookToLibrary = async (
+	bookId: number,
+	list: LibraryLists,
+): Response => {
+	const res = await post(`/book/${bookId}/library`, { list })
+}
