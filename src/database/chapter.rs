129104ce7ef915e6b9adddae95912b1ab4a8d9c3
Jonas Thiem <jonthiem@gmail.com>
2024-04-15 10:22:58 +0200

    Add chapter tests

--- src/database/chapter.rs
+++ src/database/chapter.rs
@@ -38,3 +38,46 @@ impl Database {
         .context("Unable to get chapters for book")
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[sqlx::test(fixtures("book"))]
+    async fn test_get_chapters(pool: sqlx::Pool<sqlx::Sqlite>) {
+        // Test case: Verify that get_chapters_for_book fetches chapters correctly
+        let db = Database::new_test(pool);
+
+        let chapters = db
+            .get_chapters_for_book(15)
+            .await
+            .expect("Should be able to get chapters");
+
+        assert_eq!(chapters.len(), 25);
+
+        assert_eq!(chapters[0].id, 1174);
+        assert_eq!(chapters[0].book_id, 15);
+        assert_eq!(chapters[0].name, "Opening Credits");
+        assert_eq!(chapters[0].start, 0.0);
+
+        let last_chapter = chapters.last().expect("Should be able to get last chapter");
+        assert_eq!(last_chapter.id, 1198);
+        assert_eq!(last_chapter.book_id, 15);
+        assert_eq!(last_chapter.name, "End Credits");
+    }
+
+    #[sqlx::test(fixtures("book"))]
+    async fn test_get_chapters_empty(pool: sqlx::Pool<sqlx::Sqlite>) {
+        // Test case: Verify that get_chapters_for_book reutrn nothing for a book_id that doesn't
+        // exist.
+        let db = Database::new_test(pool);
+
+        let chapters = db
+            .get_chapters_for_book(999)
+            .await
+            .expect("Should be able to get chapters");
+
+        assert_eq!(chapters.len(), 0);
+    }
+}
